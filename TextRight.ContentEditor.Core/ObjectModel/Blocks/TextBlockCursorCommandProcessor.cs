438f74e23b01e95c5dfe90e4baa300cf403fc363
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2016-02-22 21:13:47 -0600

    Consolidate code related to moving the caret in textblocks

--- TextRight.ContentEditor.Core/ObjectModel/Blocks/TextBlockCursorCommandProcessor.cs
+++ TextRight.ContentEditor.Core/ObjectModel/Blocks/TextBlockCursorCommandProcessor.cs
@@ -59,92 +59,163 @@ namespace TextRight.ContentEditor.Core.ObjectModel.Blocks
       switch (builtIn.Mode)
       {
         case BuiltInCaretNavigationCommand.NavigationType.Forward:
-          return NavigateForward(context);
+        {
+          return context.Cursor.MoveForward();
+        }
         case BuiltInCaretNavigationCommand.NavigationType.Backward:
-          return NavigateBackward(context);
-        case BuiltInCaretNavigationCommand.NavigationType.Up:
-          return NavigateUp(context);
-        case BuiltInCaretNavigationCommand.NavigationType.Down:
-          return NavigateDown(context);
+        {
+          return context.Cursor.MoveBackward();
+        }
         case BuiltInCaretNavigationCommand.NavigationType.NextWord:
-          return NavigateNextWord(context);
+        {
+          return MoveCaretToBeginningOfNextWord(context);
+        }
         case BuiltInCaretNavigationCommand.NavigationType.PreviousWord:
-          return NavigatePreviousWord(context);
+        {
+          return MoveCaretToEndOfPreviousWord(context);
+        }
+        case BuiltInCaretNavigationCommand.NavigationType.Up:
+        {
+          return TextBlockCursorMover.BackwardMover.MoveCaretTowardsPositionInNextLine(context.Cursor,
+                                                                                       context.CaretMovementMode);
+        }
+        case BuiltInCaretNavigationCommand.NavigationType.Down:
+        {
+          return TextBlockCursorMover.ForwardMover.MoveCaretTowardsPositionInNextLine(context.Cursor,
+                                                                                      context.CaretMovementMode);
+        }
+
         case BuiltInCaretNavigationCommand.NavigationType.Home:
-          return NavigateHome(context);
+        {
+          return TextBlockCursorMover.BackwardMover.MoveCaretTowardsLineEdge(context.Cursor);
+        }
         case BuiltInCaretNavigationCommand.NavigationType.End:
-          return NavigateEnd(context);
+        {
+          return TextBlockCursorMover.ForwardMover.MoveCaretTowardsLineEdge(context.Cursor);
+        }
         case BuiltInCaretNavigationCommand.NavigationType.BeginningOfBlock:
-          return NavigateBeginningofBlock(context);
+        {
+          context.Cursor.MoveToBeginning();
+          return true;
+        }
         case BuiltInCaretNavigationCommand.NavigationType.EndOfBlock:
-          return NavigateEndOfBlock(context);
+        {
+          context.Cursor.MoveToEnd();
+          return true;
+        }
         default:
           throw new ArgumentOutOfRangeException();
       }
     }
 
-    /// <inheritdoc />
-    private bool NavigateForward(DocumentEditorContext context)
+    /// <summary> Moves the caret to the beginning of the next word. </summary>
+    /// <param name="context"> The context's whose caret should be moved. </param>
+    private static bool MoveCaretToBeginningOfNextWord(DocumentEditorContext context)
     {
-      return context.Cursor.MoveForward();
-    }
+      var blockCaret = context.Cursor;
 
-    /// <inheritdoc />
-    private bool NavigateBackward(DocumentEditorContext context)
-    {
-      return context.Cursor.MoveBackward();
-    }
+      // we either don't know what kind of block cursor it is, or we want to move
+      // to the next block anyways. 
+      if (blockCaret.IsAtEnd)
+      {
+        return false;
+      }
 
-    /// <inheritdoc />
-    private bool NavigateNextWord(DocumentEditorContext context)
-    {
-      return CaretWordMover.MoveCaretToBeginningOfNextWord(context);
-    }
+      var textCursor = (TextBlock.TextBlockCursor)blockCaret;
 
-    /// <inheritdoc />
-    private bool NavigatePreviousWord(DocumentEditorContext context)
-    {
-      return CaretWordMover.MoveCaretToEndOfPreviousWord(context);
-    }
+      CharacterType characterType = Characterize(textCursor.CharacterAfter);
+      CharacterType lastCharacterType;
 
-    /// <inheritdoc />
-    private bool NavigateHome(DocumentEditorContext context)
-    {
-      return TextBlockCursorMover.BackwardMover.MoveCaretTowardsLineEdge(context.Cursor);
-    }
+      // navigate until we get to a character category that A) is different from the last
+      // seen category and B) is not an Don't-Care-Category (AKA < 0)
+      do
+      {
+        lastCharacterType = characterType;
 
-    /// <inheritdoc />
-    private bool NavigateEnd(DocumentEditorContext context)
-    {
-      return TextBlockCursorMover.ForwardMover.MoveCaretTowardsLineEdge(context.Cursor);
-    }
+        if (!textCursor.MoveForward())
+        {
+          break;
+        }
 
-    /// <inheritdoc />
-    private bool NavigateUp(DocumentEditorContext context)
-    {
-      return TextBlockCursorMover.BackwardMover.MoveCaretTowardsPositionInNextLine(context.Cursor,
-                                                                                   context.CaretMovementMode);
-    }
+        characterType = Characterize(textCursor.CharacterAfter);
+      } while (lastCharacterType == characterType
+               || characterType < CharacterType.PlannedCharacters);
 
-    /// <inheritdoc />
-    private bool NavigateDown(DocumentEditorContext context)
-    {
-      return TextBlockCursorMover.ForwardMover.MoveCaretTowardsPositionInNextLine(context.Cursor,
-                                                                                  context.CaretMovementMode);
-    }
-
-    /// <inheritdoc />
-    private bool NavigateBeginningofBlock(DocumentEditorContext context)
-    {
-      context.Cursor.MoveToBeginning();
       return true;
     }
 
-    /// <inheritdoc />
-    private bool NavigateEndOfBlock(DocumentEditorContext context)
+    /// <summary> Moves the caret to the end of the previous word. </summary>
+    /// <param name="context"> The context's whose caret should be moved. </param>
+    private static bool MoveCaretToEndOfPreviousWord(DocumentEditorContext context)
     {
-      context.Cursor.MoveToEnd();
+      var blockCaret = context.Cursor;
+
+      // we either don't know what kind of block cursor it is, or we want to move
+      // to the next block anyways. 
+      if (blockCaret.IsAtBeginning)
+        return false;
+
+      var textCursor = (TextBlock.TextBlockCursor)blockCaret;
+
+      CharacterType characterType;
+      CharacterType lastCharacterType;
+
+      // navigate backwards through all of the initial whitespace/undesirable characters
+      // until we reach a non whitespace/undesirable.
+      do
+      {
+        characterType = Characterize(textCursor.CharacterBefore);
+      } while (characterType < CharacterType.PlannedCharacters
+               && textCursor.MoveBackward());
+
+      // now move backwards until we change categories
+      do
+      {
+        lastCharacterType = characterType;
+
+        if (!textCursor.MoveBackward())
+        {
+          break;
+        }
+
+        characterType = Characterize(textCursor.CharacterBefore);
+      } while (lastCharacterType == characterType);
+
       return true;
+    }
+
+    /// <summary> Identifies the "type" of the character for analyzing groups of words. </summary>
+    /// <param name="letter"> The letter that should be categorized. </param>
+    /// <returns> The "type" of the character </returns>
+    private static CharacterType Characterize(char letter)
+    {
+      if (letter == '\'')
+      {
+        // TODO should we do this?
+        // we treat these as contractions
+        return CharacterType.Letter;
+      }
+      if (Char.IsPunctuation(letter))
+      {
+        return CharacterType.Punctuation;
+      }
+      else if (Char.IsLetter(letter))
+      {
+        return CharacterType.Letter;
+      }
+      else
+      {
+        return CharacterType.Unknown;
+      }
+    }
+
+    // NOTE the numbers are important for the above algorithms
+    private enum CharacterType
+    {
+      Unknown = -1,
+      PlannedCharacters = 0,
+      Letter = 1,
+      Punctuation = 2,
     }
   }
 }
