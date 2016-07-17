83a48700da1483139b08f024c9908237b02648df
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2016-07-16 23:43:26 -0500

    Add block descriptors which will serve as the way to determine which blocks are allowed in a document

---
+++ TextRight.ContentEditor.Core/ObjectModel/Blocks/Text/ContentBlockDescriptor.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TextRight.ContentEditor.Core.Editing;
+
+namespace TextRight.ContentEditor.Core.ObjectModel.Blocks
+{
+  /// <summary>
+  ///  Provides information about a specific type of block, allowing both creation and serialization.
+  /// </summary>
+  public abstract class ContentBlockDescriptor
+  {
+    /// <summary> The unique id of the block. </summary>
+    public abstract string Id { get; }
+
+    /// <summary> Gets the type of the block that is described by this instance. </summary>
+    public abstract Type BlockType { get; }
+
+    /// <summary> Creates a new instance of the block. </summary>
+    /// <param name="document"> The document for which the block is being created. </param>
+    /// <returns> The new instance of the block. </returns>
+    public abstract ContentBlock CreateInstance(DocumentOwner document);
+
+    /// <summary> All of the commands that should be available when the block is in a document. </summary>
+    /// <param name="document"></param>
+    public abstract IEnumerable<IContextualCommand> GetCommands(DocumentOwner document);
+  }
+
+  /// <summary> Generic version of <see cref="ContentBlockDescriptor"/> </summary>
+  /// <typeparam name="TBlock"> Type of the block being described. </typeparam>
+  public abstract class ContentBlockDescriptor<TBlock> : ContentBlockDescriptor
+    where TBlock : ContentBlock, new()
+  {
+    /// <inheritdoc />
+    public override ContentBlock CreateInstance(DocumentOwner document)
+      => new TBlock();
+
+    /// <inheritdoc />
+    public override Type BlockType
+      => typeof(TBlock);
+  }
+}
---
+++ TextRight.ContentEditor.Core/ObjectModel/Blocks/Text/ParagraphBlockDescriptor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TextRight.ContentEditor.Core.Editing;
+
+namespace TextRight.ContentEditor.Core.ObjectModel.Blocks
+{
+  /// <summary> Describes the <see cref="ParagraphBlock"/> block. </summary>
+  public class ParagraphBlockDescriptor : ContentBlockDescriptor<ParagraphBlock>
+  {
+    /// <inheritdoc />
+    public override string Id
+      => "paragraph";
+
+    /// <param name="document"></param>
+    /// <inheritdoc/>
+    public override IEnumerable<IContextualCommand> GetCommands(DocumentOwner document)
+    {
+      yield break;
+    }
+  }
+}
