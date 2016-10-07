263d476b86a59db79f04298cfa3fcc6be577b302
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2016-10-06 21:10:25 -0500

    Add a margin to headings

--- TextRight.Editor.Wpf/View/HeadingBlockView.cs
+++ TextRight.Editor.Wpf/View/HeadingBlockView.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using System.Windows.Media;
 using TextRight.ContentEditor.Core.ObjectModel;
 using TextRight.ContentEditor.Core.ObjectModel.Blocks;
@@ -18,6 +19,8 @@ namespace TextRight.ContentEditor.Desktop.View
     {
       _block = block;
       _block.Target = this;
+
+      Margin = new Thickness(10);
 
       SyncTextSize();
       Text.TextFont = new Typeface("Segoe UI Semibold");
