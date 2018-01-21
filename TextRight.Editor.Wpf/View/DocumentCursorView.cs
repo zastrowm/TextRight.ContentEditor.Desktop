747506020ddc165041fe000ab96bec93710c4f35
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2018-01-21 16:16:47 -0600

    Fix selection being hidden

--- TextRight.Editor.Wpf/View/DocumentCursorView.cs
+++ TextRight.Editor.Wpf/View/DocumentCursorView.cs
@@ -43,7 +43,7 @@ namespace TextRight.Editor.Wpf.View
                           },
                    Points = _pointCollection
                  };
-      Panel.SetZIndex(_rectangle, 10);
+      Panel.SetZIndex(_polygon, 10);
 
       _pointCollection = new PointCollection()
                          {
