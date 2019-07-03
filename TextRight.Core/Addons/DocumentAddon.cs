6fd376c5fff3beb0e4e5e7f17d113375c819d372
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2019-07-03 14:12:30 -0500

    Add basis for addon system

---
+++ TextRight.Core/Addons/DocumentAddon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TextRight.Core.Addons
+{
+  /// <summary>
+  ///  Represents a component to be added to the system, which allows extending the
+  ///  functionality of the document.
+  /// </summary>
+  public abstract class DocumentAddon
+  {
+    
+  }
+}
